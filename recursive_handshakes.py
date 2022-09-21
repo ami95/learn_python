8d8a87d8cb5e22568994715bc277e25d4fa5a0b2
EMEA\MIA1WI <Adrian.Mihai3@at.bosch.com>
2022-09-21 09:48:30 +0200

    Recursive handshakes for couples

--- recursive_handshakes.py
+++ recursive_handshakes.py
@@ -1,16 +1,28 @@
 #In a party of N people, each person will shake her/his hand with each other person only once. 
 #In total how many hand-shakes would happen?
  
-n = int(input("How many people have to shake hands?\n"))
-print(f"{n} people have to shake hands.")
+#n = int(input("How many people have to shake hands?\n"))
+#print(f"{n} people have to shake hands.")
 
 def recursive_hand_shakes(n):
     if n == 0:
         return 0
     else:
-        a = n-1
-        print(f"Person {n} shakes hand with {a} other persons")
         return n-1 + recursive_hand_shakes(n - 1)
+
+
+
+#only the men will shake hands with everyone else:
+def recursive_handshakes_couples(n):
+    global x
+    if n <= x/2:
+        return 0
+    else:
+        return (n-2) + recursive_handshakes_couples(n-1)
         
-result = recursive_hand_shakes(n)
-print(result)
+
+couples = int(input("How many couples have to shake hands?\n"))
+# since they are couples, x will always be an even number
+x=couples*2
+result = recursive_handshakes_couples(x)
+print(f"total handshakes = {result}")
